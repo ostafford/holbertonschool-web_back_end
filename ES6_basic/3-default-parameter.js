9234029f442f36222f2086ee88579c4eb077b7a9
Okky <okky.mstafford@gmail.com>
2025-03-29 15:07:10 +1100

    created files and tested

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,10 +1,3 @@
-export default function getSumOfHoods(a = initialNumber, b = expansion1989, c = expansion2019) {
-    if (b === undefined) {
-      b = 89;
-    }
-  
-    if (c === undefined) {
-      c = 19;
-    }
+export default function getSumOfHoods(a = 0, b = 89, c = 19) {
     return a + b + c;
   }
