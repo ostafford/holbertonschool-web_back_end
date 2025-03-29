3983c385053afc2ad62382aa792aeaca335d7d31
Okky <okky.mstafford@gmail.com>
2025-03-30 09:45:06 +1100

    created files and tested

---
+++ ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,8 @@
+export default function createEmployeesObject(departmentName, employees) {
+    const empArray = [];
+
+    for (const idx of employees) {
+        empArray.push(idx)
+    }
+    return `${departmentName}:` + empArray;
+}
