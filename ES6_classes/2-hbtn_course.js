de6060a9d3cfdad3e7fb154aeeaa213e0ce96ca3
Okky <okky.mstafford@gmail.com>
2025-04-05 12:32:59 +1100

    created files

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,54 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+
+        // property/attribute check for data type.
+      if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      this._name = name;
+      
+      if (typeof length !== 'number') {
+        throw new TypeError('Length must be a number');
+      }
+      this._length = length;
+      
+      if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+        throw new TypeError('Students must be an array of strings');
+      }
+      this._students = students;
+    }
+  
+    // Getter and setters 
+    get name() {
+      return this._name;
+    }
+  
+    set name(newName) {
+      if (typeof newName !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      this._name = newName;
+    }
+  
+    get length() {
+      return this._length;
+    }
+  
+    set length(newLength) {
+      if (typeof newLength !== 'number') {
+        throw new TypeError('Length must be a number');
+      }
+      this._length = newLength;
+    }
+  
+    get students() {
+      return this._students;
+    }
+  
+    set students(newStudents) {
+      if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+        throw new TypeError('Students must be an array of strings');
+      }
+      this._students = newStudents;
+    }
+  }
