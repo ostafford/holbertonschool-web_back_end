0da94295407d74161c2392ed5e5717ab44bb4c80
Okky <okky.mstafford@gmail.com>
2025-04-05 17:06:43 +1100

    created files

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,22 +1,19 @@
 export default class Building{
     constructor(sqft){
-
-        if (this.constructor === Building){
-            throw new Error("Cannot create instance directly");
-            }
-
         this._sqft = sqft;
-    }
 
-    
-    get sqft(){
-        return this._sqft;
+        if (this.constructor !== Building){
+            }
+        }
+        
+        get sqft(){
+            return this._sqft;
+        }
+        set sqft(newSqft){
+            return this._sqft = newSqft;
+        }
+        
+        evacuationWarningMessage(){
+                throw new Error("Class extending Building must override evacuationWarningMessage");
+            }
     }
-    set sqft(newSqft){
-        return this._sqft = newSqft;
-    }
-
-    evacuationWarningMessage() {
-        throw new Error("Class extending Building must override evacuationWarningMessage");
-      }
-}
