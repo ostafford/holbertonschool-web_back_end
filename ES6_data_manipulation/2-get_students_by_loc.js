78cae4a2e9c49aedd54d58bcb69dcf6697496454
Okky <okky.mstafford@gmail.com>
2025-04-07 16:06:25 +1000

    created files

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,7 @@
 export default function getStudentsByLocation(getListStudents, city) {
-  if (!Array.isArray(getListStudents, city)) {
+  if (!Array.isArray(getListStudents)) {
     return [];
   }
 
-  return getListStudents.filter(students => students.location === 'San Francisco');
+  return getListStudents.filter(students => students.location === city);
 }
