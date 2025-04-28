d4d86b08bd86af8d50fbe244e6c7c9f7f9d9a546
Okky <okky.mstafford@gmail.com>
2025-04-28 14:18:25 +1000

    added and created required files

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float (largest integer less than or equal to n).
+    
+    Args:
+        n: A floating point number
+        
+    Returns:
+        The floor of n as an integer
+    """
+    import math
+    return math.floor(n)
