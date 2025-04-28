6a842ee165afd3f89542d59e0919b8169e0cc0fd
Okky <okky.mstafford@gmail.com>
2025-04-28 15:38:15 +1000

    added and created required files

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-def make_multiplier(multiplier: float) -> callable(float):
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Create and returns function that multiples float by fixed multiplier"""
     def multiply_by_multiplier(n: float) -> float:
         return multiplier * n
     return multiply_by_multiplier
